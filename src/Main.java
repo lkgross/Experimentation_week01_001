ee81e940fde4193a6f112db8d0ff89fba3b4737b
L1Gross <profgross@gmail.com>
2023-01-23 14:17:01 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * This is the Main class in the Java program.
+ */
+public class Main {
+    // The class header: public class Main
+    /*
+     * Think of the Main class as a container for the Java program.
+     * Note the syntax for a multi-line comment.
+     */
+    /**
+     * This is the main method in the Java program.
+     */
+    public static void main(String[] args) {
+        // The method header: public static void main(String[] args)
+        System.out.print("Hello world!\n"); // Note print does not move to the next line.
+        // The println command moves down to the next line.
+        System.out.println("\tHello Tatooine!");
+        System.out.print("\n");
+        System.out.println("Hello 'Tatooine'!");
+        System.out.println("Hello\\Goodbye \"Alderan\"!");
+    }
+}
